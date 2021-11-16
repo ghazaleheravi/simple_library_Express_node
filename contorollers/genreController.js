1d2c42342b23983aa9f0f5de0b86bfe72f842b8f
Ghazaleh Heravi <ghazaleheravi@users.noreply.github.com>
2021-11-15 23:24:26 -0500

    genre_list routerhandler described + viewed

--- contorollers/genreController.js
+++ contorollers/genreController.js
@@ -1,8 +1,12 @@
 var Genre = require('../models/genre');
 
 // Display list of all Genre.
-exports.genre_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre list');
+exports.genre_list = function(req, res, next) {
+  Genre.find()
+    .exec(function(err, results) {
+        if(err) {return next(err);}
+        res.render('genre_list', {title: 'Genres List:', genre_list: results});
+    })    
 };
 
 // Display detail page for a specific Genre.
